82eacf5c0ab7156a6ae781a713f1f8e69aaa1b5c
jonathan diaz <jonathandiaz0914@gmail.com>
2020-03-21 14:59:20 -0400

    added search routes with filter

--- controllers/product.js
+++ controllers/product.js
@@ -129,3 +129,102 @@ exports.update = (req,res) => {
         })
     })
 }
+
+
+//Sell / Arrival
+
+//* by sell = /products?sortBy=sold&order=desc&limit=4
+
+// by arrival = /products?sortBy=createdAt&order=desc&limit=4
+
+
+exports.list = (req,res) => {
+    let order = req.query.order ? req.query.order: 'asc'
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id'
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+
+    
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                // gte -  greater than price [0-10]
+                // lte - less than
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+ 
+
+    Product.find()
+    .select("-photo")
+    .populate('category')
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err,products) => {
+        if(err){
+            return res.status(400).json({
+                message: "product not found"
+            })
+        }
+        res.send(products)
+    })
+
+}
+
+exports.listRelated = (req,res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+
+    Product.find({_id: {$ne: req.product}, category: req.product.category})
+    .limit(limit)
+    .populate('category', '_id name')
+    .exec((err, product) => {
+        if(err){
+            return res.status(400).json({
+                message: "product not found"
+            })
+        }
+
+        res.json(products)
+    })
+}
+
+exports.listCategories = (req,res) => {
+    Product.distinct("category", {}, (err, product) => {
+        if(err){
+            return res.status(400).json({
+                message: "Categories not found"
+            })
+        }
+        res.json(product)
+    })
+}
+exports.listBySearch = (req,res) => {
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
+
+    Product.find(findArgs)
+    .select("-photo")
+    .populate("category")
+    .sort([[sortBy, order]])
+    .skip(skip)
+    .limit(limit)
+    .exec((err, data) => {
+        if(err){
+            return res.status(400).json({
+                message: "Categories not found"
+            })
+        }
+        res.json({
+            size: data.length,
+            data
+        })
+    })
+}
--- routes/product.js
+++ routes/product.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { create} = require("../controllers/product");
+
 
 const {requireSignin, isAuth, isAdmin} = require("../controllers/auth")
 const { userById } = require("../controllers/user")
-const { productById, read, remove, update} = require("../controllers/product")
+const { productById, read, remove, update, create, list, listRelated, listCategories, listBySearch} = require("../controllers/product")
 
 router.get("/product/:productId", read)
 router.post('/product/create/:userId' ,requireSignin, isAdmin, isAuth , create);
 router.delete("/product/:productId/:userId", requireSignin, isAdmin, isAuth , remove)
 router.put("/product/:productId/:userId", requireSignin, isAdmin, isAuth , update)
-
-
+router.get("/products", list)
+router.get("/products/related/:productId", listRelated)
+router.get("/products/categories", listCategories)
+router.post("/product/by/search", listBySearch)
 
 router.param("userId", userById)
 router.param("productId", productById)
